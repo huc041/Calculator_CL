5bcfbd61fc5fab91423325e55a56509ec5fdae73
Tanya <tanya@MacBook-Air-Tanya.local>
2016-08-07 14:46:09 +0300

    вынес печати в отдельную функцию, доделка всех матем операций

---
+++ Calculator_CL/help.h
@@ -0,0 +1,14 @@
+//
+//  help.h
+//  Calculator_CL
+//
+//  Created by Tanya on 07.08.16.
+//  Copyright © 2016 Евгений Иванов. All rights reserved.
+//
+
+#ifndef help_h
+#define help_h
+
+void printOnScreen(float num);
+
+#endif /* help_h */
--- Calculator_CL/main.m
+++ Calculator_CL/main.m
@@ -8,47 +8,67 @@
 
 #import <Foundation/Foundation.h>
 #import "stdio.h"
+#import "help.h"
+
+void printOnScreen(float num) {
+    NSLog(@"Результат = %.1f", num);
+}
+
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         
-        NSLog(@"Введите первый операнд \n");
+        char str[1];
         float firstOperand = 0.0;
-        scanf("%f", &firstOperand);
-        
-        NSLog(@"Введите второй операнд \n");
         float secondOperand = 0.0;
-        scanf("%f", &secondOperand);
+        scanf("%s %f %f", str,&firstOperand,&secondOperand);
         
-        NSLog(@"Введите номер команды: 1 - сложение, 2 - вычитание, 3 - умножение, 4 - деление");
-        int commandNum = 0;
-        scanf("%d", &commandNum);
+        NSString *strCommand = [NSString stringWithFormat:@"%s",str];
         
-        switch (commandNum) {
-            case 1:
-                NSLog(@"Результат = %f", firstOperand + secondOperand);
-                break;
-            case 2:
-                NSLog(@"Результат = %f", firstOperand - secondOperand);
-                break;
-            case 3:
-                NSLog(@"Результат = %f", firstOperand * secondOperand);
-                break;
-            case 4:
-            {
-                if(secondOperand !=0)
-                    NSLog(@"Результат = %f", firstOperand / secondOperand);
-                else
-                    NSLog(@"Ahtung! Деление на нуль!");
-                break;
-            }
-            default:
-                NSLog(@"Недопустимая команда!");
-                break;
+        if([strCommand isEqualToString:@"+"]){
+            printOnScreen(firstOperand + secondOperand);
         }
+        else if ([strCommand isEqualToString:@"-"]){ // вычитание
+            printOnScreen(firstOperand - secondOperand);
+        }
+        else if ([strCommand isEqualToString:@"*"]){ // умножение
+            printOnScreen(firstOperand * secondOperand);
+        }
+        else if ([strCommand isEqualToString:@"/"]){ // деление
+            
+            if(secondOperand !=0)
+                printOnScreen(firstOperand / secondOperand);
+            else
+                NSLog(@"Ошибка! Деление на нуль!");
+        }
+        else if ([strCommand isEqualToString:@"%"]){ // остаток от деления
+            
+            if(secondOperand !=0)
+            {
+                int result  = (int)((int)firstOperand % (int)secondOperand);
+                printOnScreen((float)result);
+            }
+            else
+                NSLog(@"Ошибка! Деление на нуль!");
+        }
+        else if ([strCommand isEqualToString:@"Q"]){ // корень степени OPERAND1 из OPERAND2
+            
+            if(firstOperand !=0)
+                printOnScreen(powf(secondOperand, (1/firstOperand)));
+            else
+                NSLog(@"Ошибка! Деление на нуль!");
+        }
+        else{
+            NSLog(@"Ошибка! Недопустимая команда!");
+        }
+        
+        
     }
     return 0;
 }
+
+
+
 
 
